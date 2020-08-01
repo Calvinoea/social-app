583dc05335c9ebfbc7c8b2b3712474ffa349e11e
calvinoea <calvinebunamu@gmail.com>
2020-08-01 20:18:50 +0100

    Changes made to view. Backend connected to frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+Vue.component('like-component', require('./components/LikeComponent.vue').default);
+Vue.component('dis-like-component', require('./components/DisLikeComponent.vue'));
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
